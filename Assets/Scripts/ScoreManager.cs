af2e61189e15907ab9e27a1ca476f296bf377aad
RikiDeh <riki24226@gmail.com>
2026-07-07 22:57:51 +0700

    adding restart menu

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement; 
 
 public class ScoreManager : MonoBehaviour
 {
@@ -12,11 +13,17 @@ public class ScoreManager : MonoBehaviour
     private int timeLeft = 60;
     private int score = 0;
 
+    [Header("Game Over UI Setup")]
+    public GameObject panelGameOver; 
+    public TextMeshProUGUI totalScoreText;
+    private bool gameHasEnded = false;
+
     void Awake()
     {
         if (instance == null)
         {
             instance = this; 
+            Time.timeScale = 1f; 
         }
         else
         {
@@ -24,16 +31,40 @@ public class ScoreManager : MonoBehaviour
         }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         scoreText.text = "Score     : " + score.ToString();
         timeText.text = "Time Left  : " + timeLeft.ToString();
+
+        StartCoroutine(CountdownWaktu());
+    }
+    IEnumerator CountdownWaktu()
+    {
+        while (timeLeft > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            timeLeft--;
+            timeText.text = "Time Left  : " + timeLeft.ToString(); 
+        }
+        GameOver();
     }
 
     public void AddScore(int points)
     {
+        if (gameHasEnded) return;
+
         score += points;
         scoreText.text = "Score     : " + score.ToString();
+    }
+    void GameOver()
+    {
+        gameHasEnded = true;
+        totalScoreText.text = "Total Score: " + score.ToString();
+        panelGameOver.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
